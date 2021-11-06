ac08b7323d75f040f09f208fc8321b91f57386ca
Zohaib Tahir <Zohaibtahiruaf@gmail.com>
2021-11-06 10:27:38 +0500

    Create Scientific-notation-to-real_number.js

---
+++ Scientific-notation-to-real_number.js
@@ -0,0 +1,34 @@
+// This function is for converting the expression like 2.345*10^4 in to simple 23450
+/*
+Developer: Zohaib Tahir
+Linkedin: zohaibtahir24
+
+*/
+
+function SNtoRealNumber(num){
+    if(num.includes('*') && num.includes('^')){
+		var splited_num_array = num.split('*');
+		var float_number = splited_num_array[0];
+		var power_number_array = splited_num_array[1].split('^');
+		var power_number = power_number_array[1];
+		if(power_number.includes('+')){
+			var simple_p_number = power_number.split('+');
+			var power_ten = '1';
+			for(let i=0; i<simple_p_number[1];i++){
+				power_ten += 0;
+			}
+			final_number = Number(float_number * power_ten).toPrecision();
+		}
+		if(power_number.includes('-')){
+			var simple_p_number = power_number.split('-');
+			var power_ten = '1';
+			for(let i=0; i<simple_p_number[1];i++){
+				power_ten += 0;
+			}
+			final_number = float_number / power_ten;
+		}
+        return final_number;
+    }else{
+        return num;
+    }
+} 
